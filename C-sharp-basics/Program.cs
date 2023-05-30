c459864b08e1d546a5d72c0bf82d2daa873144f6
omranali2111 <aljabri1234@hotmail.com>
2023-05-30 11:14:22 +0400

    basic c sharp code

---
+++ C-sharp-basics/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Name: Omran");
+        Console.WriteLine("Father Name: Ali");
+        Console.WriteLine("DOB: 1996-01-29");
+        Console.WriteLine("Email: aljabri1234@hotmail.com");
+        Console.WriteLine("GitHub UserName: omranali2111");
+    }
+}
